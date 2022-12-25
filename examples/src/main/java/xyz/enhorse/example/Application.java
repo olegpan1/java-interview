ad4c14b15d117887a644ee0670ca2eab54308566
olegpan1 <olegpan@hotmail.com>
2022-12-25 12:50:15 +0700

    new book

--- examples/src/main/java/xyz/enhorse/example/Application.java
+++ examples/src/main/java/xyz/enhorse/example/Application.java
@@ -3,6 +3,6 @@ package xyz.enhorse.example;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Examples for Java Developer interview preparation materials!");
+        System.out.println("Examples for  Java Developer interview preparation materials!");
     }
 }
